c4669fc151a0eee9c08bc26e7cfe7c64a116afe6
superben486 <ben@thesupers.org>
2021-12-05 22:03:19 -0500

    Added initial routing implementation (does not work yet)

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as Leaflet from 'leaflet';
+import 'leaflet-routing-machine';
 
 @Component({
   selector: 'app-map',
@@ -26,5 +27,48 @@ export const getLayers = (): Leaflet.Layer[] => {
     new Leaflet.TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; OpenStreetMap contributors'
     } as Leaflet.TileLayerOptions),
+    //Leaflet.layerGroup(getMarkers()),
+    getRouting(),
   ] as Leaflet.Layer[];
+};
+
+export const getMarkers = (): Leaflet.Marker[] => {
+  return [
+    new Leaflet.Marker(new Leaflet.LatLng(42.3936964, -72.5311701), {
+      icon: new Leaflet.Icon({
+        iconSize: [50, 41],
+        iconAnchor: [13, 41],
+        iconUrl: 'assets/point-marker-1.svg',
+      }),
+      title: 'Workspace'
+    } as Leaflet.MarkerOptions),
+    new Leaflet.Marker(new Leaflet.LatLng(42.3976964, -72.5341701), {
+      icon: new Leaflet.Icon({
+        iconSize: [50, 41],
+        iconAnchor: [13, 41],
+        iconUrl: 'assets/point-marker-2.svg',
+      }),
+      title: 'Riva'
+    } as Leaflet.MarkerOptions),
+  ] as Leaflet.Marker[];
+};
+
+export const getRoutes = (): Leaflet.Polyline[] => {
+  return [
+    new Leaflet.Polyline([
+      new Leaflet.LatLng(42.3936964, -72.5311701),
+      new Leaflet.LatLng(42.3976964, -72.5341701),
+    ] as Leaflet.LatLng[], {
+      color: '#0d9148'
+    } as Leaflet.PolylineOptions)
+  ] as Leaflet.Polyline[];
+};
+
+export const getRouting = (): Leaflet.Routing.Control => {
+  return new Leaflet.Routing.Control({
+    waypoints: [
+      new Leaflet.LatLng(42.3936964, -72.5311701),
+      new Leaflet.LatLng(42.3976964, -72.5341701),
+    ]
+  })
 };
