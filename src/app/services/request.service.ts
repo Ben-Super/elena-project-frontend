c3a541b8ef73908c0796bfee0bfd39036ae254b3
Ben-Super <superben486@gmail.com>
2021-12-12 23:14:42 -0500

    Cleaned up some imports

--- src/app/services/request.service.ts
+++ src/app/services/request.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { PathData } from '../templates/pathdata';
 
 @Injectable({
