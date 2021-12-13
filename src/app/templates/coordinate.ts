533ff8d0365666f2c6314755afac5c37af0584f4
Ben-Super <superben486@gmail.com>
2021-12-13 12:00:10 -0500

    Encapsulated members of the coordinate class

--- src/app/templates/coordinate.ts
+++ src/app/templates/coordinate.ts
@@ -2,12 +2,28 @@
  * This class represents a single lat/lng coordinate
  */
 export class Coordinate {
-    public lat: number;
-    public long: number;
+    private lat: number;
+    private long: number;
 
     constructor(lat: number, long: number) {
         this.lat = lat;
         this.long = long
+    }
+
+    public setLat(lat: number): void {
+        this.lat = lat;
+    }
+
+    public setLong(long: number): void {
+        this.long = long;
+    }
+
+    public getLat(): number {
+        return this.lat;
+    }
+
+    public getLong(): number {
+        return this.long;
     }
 
     public toString(): string {
